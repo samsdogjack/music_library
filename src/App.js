81118024ccdf5503e97b4c69edbba9fd8bf92444
samsdogjack <105887707+samsdogjack@users.noreply.github.com>
2022-10-29 11:11:31 -0400

    fix typo

--- src/App.js
+++ src/App.js
@@ -1,36 +1,46 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
+import { DataContext } from './context/DataContext'
+//import { SearchContext } from './src/context/searchContext'
+const App = () => {
+  let [search, setSearch] = useState('')
+  let [message, setMessage] = useState('Search for Music!')
+  let [data, setData] = useState([])
 
-function App(){
-    let [search, setSearch] = useState('')
-    let [message, setMessage] = useState('Search for Music!')
-    let [data, setData] = useState([])
+  const API_URL = 'https://itunes.apple.com/search?term='
 
-    useEffect(() => {
+  useEffect(() => {
+    if (search) {
       const fetchData = async () => {
-          document.title = `${search} Music`
-          const response = await fetch('https://itunes.apple.com/search?term=the%20grateful%20dead')
-          const resData = await response.json()
-          if (resData.results.length > 0) {
-            setData(resData.results)
+        document.title = `${search} music`
+        const response = await fetch(API_URL + search)
+        const resData = await response.json()
+        if (resData.results.length > 0) {
+          return setData(resData.results)
         } else {
-            setMessage('Not Found')
+          return setMessage('Not Found.')
         }
-
       }
       fetchData()
-    }, [search])
-    
-    
-    return (
-      <div>
-            <SearchBar />
-            {message}
-            <Gallery />
-        </div>
-    )
+    }
+  }, [search])
+
+  const handleSearch = (e, term) => {
+    e.preventDefault()
+    setSearch(term)
   }
-  
-  //console.log(resData)
-  export default App
+
+  return (
+    <div className="App">
+      <SearchBar handleSearch={handleSearch} />
+      {message}
+      <DataContext.Provider value={data} >
+        <Gallery />
+      </DataContext.Provider>
+    </div>
+  )
+}
+
+
+export default App
