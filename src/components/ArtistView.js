2291541afd3d34c5d3188f72218214eb87b3c643
samsdogjack <105887707+samsdogjack@users.noreply.github.com>
2022-10-31 21:22:02 -0400

    changes in ArtistView

--- src/components/ArtistView.js
+++ src/components/ArtistView.js
@@ -1,44 +1,48 @@
-import {useEffect, useState} from 'react'
-import {useParams, Link, useHistory} from 'react-router-dom'
+import { useEffect, useState } from "react";
+import { useParams, Link, useHistory } from "react-router-dom";
 
 const ArtistView = () => {
-    const { id } = useParams()
-    const [ artistData, setArtistData ] = useState([])
-    const history = useHistory()
-    
-    useEffect(() => {
-        const API_URL = `http://localhost:4000/album/${id}`
-        const fetchData = async () => {
-            const response = await fetch(API_URL)
-            const resData = await response.json()
-            setArtistData(resData.results)
-        }
-        fetchData()
-    }, [id])
+  const { id } = useParams();
+  const [artistData, setArtistData] = useState([]);
+  const history = useHistory();
 
-    const allAlbums = artistData.filter(entity => entity.collectionType === 'Album')
-                        .map((album, i) => { 
-                            return (
-                                <div key={i}>
-                                    <Link to={`/album/${album.collectionId}`}>
-                                        <p>{album.collectionName}</p>
-                                    </Link>
-                                </div>)
-                                })
-    
+  useEffect(() => {
+    const API_URL = `http://localhost:4000/album/${id}`;
+    const fetchData = async () => {
+      const response = await fetch(API_URL);
+      const resData = await response.json();
+      setArtistData(resData.results);
+    };
+    fetchData();
+  }, [id]);
+
+  const justAlbums = artistData.filter(entry => entry.collectionType === 'Album')
+
+    const renderAlbums = justAlbums.map((album, i) => {
+        return (
+            <div key={i}>
+                <Link to={`/album/${album.collectionId}`}>
+                    <p>{album.collectionName}</p>
+                </Link>
+            </div>
+        )
+    })
+
     const navButtons = () => {
         return (
             <div>
-                <button onClick={() => {history.push('/')}}>Home</button> | <button onClick={() => history.goBack()}>Back</button>
+                <button onClick={() => navigate(-1)}>Back</button>
+                |
+                <button onClick={() => navigate('/')}>Home</button>
             </div>
         )
     }
 
     return (
         <div>
-            {artistData.length > 0 ? <h2>{artistData[0].artistName}</h2> : <p>loading...</p>}
+            {artistData.length > 0 ? <h2>{artistData[0].artistName}</h2> : <h2>Loading...</h2>}
             {navButtons()}
-            {allAlbums}
+            {renderAlbums}
         </div>
     )
 }
